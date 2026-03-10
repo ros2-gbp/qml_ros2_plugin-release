29ef5c9190cea84446f583e33eb9898a6bce5372
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2026-03-10 06:16:50 +0000

    Rebase from 'release/humble/qml_ros2_plugin'

--- include/qml_ros2_plugin/image_transport_manager.hpp
+++ include/qml_ros2_plugin/image_transport_manager.hpp
@@ -12,6 +12,11 @@ namespace qml_ros2_plugin
 {
 class ImageTransportSubscriptionHandle;
 
+struct ImageInformation {
+  rclcpp::Time timestamp;
+  std::string encoding;
+};
+
 /*!
  * Encapsulates the image transport communication to share the subscription resources, avoiding multiple conversions of
  *  the same image and subscription overhead if multiple cameras are set to throttle.
@@ -25,28 +30,28 @@ class ImageTransportManager
   class Subscription;
 
 public:
+  using ImageCallback = std::function<void( const QVideoFrame &, const ImageInformation & )>;
   static ImageTransportManager &getInstance();
 
   /*!
    * Note: Can only be called with a ready NodeHandle!
    *
-   * Subscribes to the given topic with the given settings. Makes sure that multiple subscriptions_ (especially throttled)
-   *  only result in a single (throttled) subscription.
-   * If multiple subscriptions_ of the same topic and namespace are created, the settings of the first subscription are used.
-   * Except for the throttle interval where the minimum value across all active subscriptions_ is used.
-   * @param qtopic
-   * @param queue_size
-   * @param transport_hints
-   * @param callback
-   * @param surface
-   * @param throttle_interval
-   * @return
+   * Subscribes to the given topic with the given settings. Makes sure that multiple subscriptions
+   *  only result in a single subscription.
+   * If multiple subscriptions of the same topic and namespace are created, the settings of the
+   * first subscription are used.
    */
+  std::shared_ptr<ImageTransportSubscriptionHandle>
+  subscribe( const rclcpp::Node::SharedPtr &node, const QString &qtopic, quint32 queue_size,
+             const image_transport::TransportHints &transport_hints, const ImageCallback &callback,
+             const QList<QVideoFrame::PixelFormat> &supported_pixel_formats );
+
+  // Overload for backwards compatibility. Is removed in a Qt6 version.
   std::shared_ptr<ImageTransportSubscriptionHandle>
   subscribe( const rclcpp::Node::SharedPtr &node, const QString &qtopic, quint32 queue_size,
              const image_transport::TransportHints &transport_hints,
              const std::function<void( const QVideoFrame & )> &callback,
-             QAbstractVideoSurface *surface = nullptr );
+             const QList<QVideoFrame::PixelFormat> &supported_pixel_formats );
 
 private:
   std::shared_ptr<SubscriptionManager> subscription_manager_;
@@ -76,8 +81,8 @@ public:
 
 private:
   std::shared_ptr<ImageTransportManager::Subscription> subscription;
-  QAbstractVideoSurface *surface = nullptr;
-  std::function<void( const QVideoFrame & )> callback;
+  QList<QVideoFrame::PixelFormat> supported_pixel_formats;
+  ImageTransportManager::ImageCallback callback;
   double framerate_ = 0;
   int network_latency = -1;
   int processing_latency = -1;
--- src/service_client.cpp
+++ src/service_client.cpp
@@ -2,9 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "qml_ros2_plugin/service_client.hpp"
+#include "logging.hpp"
 #include "qml_ros2_plugin/babel_fish_dispenser.hpp"
 #include "qml_ros2_plugin/conversion/message_conversions.hpp"
-#include "qml_ros2_plugin/helpers/logging.hpp"
 #include "qml_ros2_plugin/ros2.hpp"
 
 #include <QJSEngine>
@@ -15,10 +15,20 @@ using namespace qml_ros2_plugin::conversion;
 
 namespace qml_ros2_plugin
 {
-ServiceClient::ServiceClient( QString name, QString type )
-    : name_( std::move( name ) ), service_type_( std::move( type ) )
+ServiceClient::ServiceClient( QString name, QString type, const QoSWrapper &qos )
+    : qos_( qos ), name_( std::move( name ) ), service_type_( std::move( type ) )
 {
   babel_fish_ = BabelFishDispenser::getBabelFish();
+  connect_timer_.setInterval( 16 );
+  connect_timer_.setSingleShot( false );
+  connect( &connect_timer_, &QTimer::timeout, this, &ServiceClient::checkServiceReady );
+}
+
+ServiceClient::~ServiceClient()
+{
+  std::unique_lock lock( check_ready_mutex_ );
+  connect_timer_.stop();
+  disconnect( &connect_timer_, &QTimer::timeout, this, &ServiceClient::checkServiceReady );
 }
 
 void ServiceClient::onRos2Initialized()
@@ -26,15 +36,21 @@ void ServiceClient::onRos2Initialized()
   try {
     rclcpp::Node &node = *Ros2Qml::getInstance().node();
     client_ =
-        babel_fish_.create_service_client( node, name_.toStdString(), service_type_.toStdString() );
+        babel_fish_.create_service_client( node, name_.toStdString(), service_type_.toStdString(),
+                                           qos_.rclcppQoS().get_rmw_qos_profile() );
   } catch ( BabelFishException &ex ) {
     QML_ROS2_PLUGIN_ERROR( "Could not create ServiceClient: %s", ex.what() );
     client_ = nullptr;
     return;
+  } catch ( std::exception &ex ) {
+    QML_ROS2_PLUGIN_ERROR( "Could not create ServiceClient: %s", ex.what() );
+    client_ = nullptr;
+    return;
+  } catch ( ... ) {
+    QML_ROS2_PLUGIN_ERROR( "Could not create ServiceClient: Unknown error." );
+    client_ = nullptr;
+    return;
   }
-  connect_timer_.setInterval( 16 );
-  connect_timer_.setSingleShot( false );
-  connect( &connect_timer_, &QTimer::timeout, this, &ServiceClient::checkServiceReady );
   connect_timer_.start();
 }
 
@@ -42,11 +58,50 @@ void ServiceClient::onRos2Shutdown() { client_.reset(); }
 
 void ServiceClient::checkServiceReady()
 {
-  if ( !isServiceReady() )
+  std::unique_lock lock( check_ready_mutex_ );
+  if ( !isServiceReady() ) {
+    int timeouted = 0;
+    for ( size_t i = 0; i < waiting_service_calls_.size(); ++i, ++timeouted ) {
+      if ( clock::now() - waiting_service_calls_[i].start <
+           std::chrono::milliseconds( connection_timeout_ ) )
+        break;
+      QML_ROS2_PLUGIN_DEBUG(
+          "Request for service '%s' timeouted while waiting for it to become ready.",
+          name_.toStdString().c_str() );
+      pending_requests_--;
+      invokeCallback( waiting_service_calls_[i].id, false );
+    }
+    if ( timeouted > 0 ) {
+      waiting_service_calls_.erase( waiting_service_calls_.begin(),
+                                    waiting_service_calls_.begin() + timeouted );
+      emit pendingRequestsChanged();
+    }
     return;
+  }
+  QML_ROS2_PLUGIN_DEBUG( "Service '%s' is ready.", name_.toStdString().c_str() );
   connect_timer_.stop();
-  disconnect( &connect_timer_, &QTimer::timeout, this, &ServiceClient::checkServiceReady );
   emit serviceReadyChanged();
+
+  if ( waiting_service_calls_.empty() )
+    return;
+
+  blockSignals( true );
+  for ( const auto &call : waiting_service_calls_ ) {
+    pending_requests_--;
+    auto it = pending_callbacks_.find( call.id );
+    if ( it == pending_callbacks_.end() ) {
+      QML_ROS2_PLUGIN_ERROR(
+          "ServiceClient: Could not find pending callback with ID %d in checkServiceReady. Can not "
+          "send request after connection was established.",
+          call.id );
+      continue;
+    }
+    sendRequestAsync( call.request, it->second );
+    pending_callbacks_.erase( it );
+  }
+  waiting_service_calls_.clear();
+  blockSignals( false );
+  emit pendingRequestsChanged();
 }
 
 bool ServiceClient::isServiceReady() const
@@ -58,37 +113,98 @@ const QString &ServiceClient::name() const { return name_; }
 
 const QString &ServiceClient::type() const { return service_type_; }
 
+int ServiceClient::connectionTimeout() const { return connection_timeout_; }
+
+void ServiceClient::setConnectionTimeout( int timeout )
+{
+  if ( connection_timeout_ == timeout )
+    return;
+  connection_timeout_ = timeout;
+  emit connectionTimeoutChanged();
+}
+
+int ServiceClient::pendingRequests() const { return pending_requests_; }
+
 void ServiceClient::sendRequestAsync( const QVariantMap &req, const QJSValue &callback )
 {
-  if ( client_ == nullptr ) {
-    QML_ROS2_PLUGIN_ERROR( "Tried to send goal when ServiceClient was not connected!" );
-    if ( !callback.isCallable() )
-      return;
-    QMetaObject::invokeMethod( this, "invokeCallback", Qt::AutoConnection,
-                               Q_ARG( QJSValue, callback ), Q_ARG( QVariant, QVariant( false ) ) );
+  if ( !engine_ ) {
+    engine_ = qjsEngine( this );
+  }
+  pending_requests_++;
+  int callback_id = generateInternalCallbackId();
+  while ( pending_callbacks_.find( callback_id ) != pending_callbacks_.end() ) {
+    callback_id = generateInternalCallbackId();
+  }
+  pending_callbacks_[callback_id] = callback;
+  if ( !isServiceReady() ) {
+    QML_ROS2_PLUGIN_DEBUG( "Service '%s' not ready, waiting up to %d ms.",
+                           name_.toStdString().c_str(), connection_timeout_ );
+
+    waiting_service_calls_.push_back( { req, callback_id, clock::now() } );
+    emit pendingRequestsChanged();
+    if ( !connect_timer_.isActive() ) {
+      // If the time is not active, the service was ready at some point and is not ready anymore.
+      emit serviceReadyChanged();
+      connect_timer_.start();
+    }
     return;
   }
+  QML_ROS2_PLUGIN_DEBUG( "Service '%s' is ready. Sending request.", name_.toStdString().c_str() );
   CompoundMessage::SharedPtr message;
   try {
     message = babel_fish_.create_service_request_shared( service_type_.toStdString() );
     fillMessage( *message, req );
+    QPointer instance = this;
     client_->async_send_request(
-        message, [callback, this]( BabelFishServiceClient::SharedFuture response ) {
-          QMetaObject::invokeMethod( this, "invokeCallback", Qt::AutoConnection,
-                                     Q_ARG( QJSValue, callback ),
+        message, [callback_id, instance]( BabelFishServiceClient::SharedFuture response ) {
+          if ( !instance )
+            return;
+          QML_ROS2_PLUGIN_DEBUG( "Received response from service %s.",
+                                 instance->name_.toStdString().c_str() );
+          instance->pending_requests_--;
+          emit instance->pendingRequestsChanged();
+          QMetaObject::invokeMethod( instance, "invokeCallback", Qt::AutoConnection,
+                                     Q_ARG( int, callback_id ),
                                      Q_ARG( QVariant, msgToMap( response.get() ) ) );
         } );
+    emit pendingRequestsChanged();
+    return;
   } catch ( BabelFishException &ex ) {
     QML_ROS2_PLUGIN_ERROR( "Failed to call service: %s", ex.what() );
-    QMetaObject::invokeMethod( this, "invokeCallback", Qt::AutoConnection,
-                               Q_ARG( QJSValue, callback ), Q_ARG( QVariant, QVariant( false ) ) );
-    return;
+  } catch ( std::exception &ex ) {
+    QML_ROS2_PLUGIN_ERROR( "Failed to call service: %s", ex.what() );
+  } catch ( ... ) {
+    QML_ROS2_PLUGIN_ERROR( "Failed to call service: Unknown error." );
   }
+  pending_requests_--;
+  invokeCallback( callback_id, false );
 }
 
-void ServiceClient::invokeCallback( QJSValue value, QVariant result )
+void ServiceClient::invokeCallback( int id, const QVariant &result )
 {
-  QJSEngine *engine = qjsEngine( this );
-  value.call( { engine->toScriptValue( result ) } );
+  QJSEngine *engine = engine_ ? engine_.data() : qjsEngine( this );
+  if ( !engine ) {
+    QML_ROS2_PLUGIN_ERROR(
+        "ServiceClient: Failed to get QJSEngine in invokeCallback. Can not invoke callback." );
+    return;
+  }
+  auto it = pending_callbacks_.find( id );
+  if ( it == pending_callbacks_.end() ) {
+    QML_ROS2_PLUGIN_ERROR(
+        "ServiceClient: Could not find pending callback with ID %d in invokeCallback. Can not "
+        "invoke callback.",
+        id );
+    return;
+  }
+  QJSValue &callback = it->second;
+  callback.call( { engine->toScriptValue( result ) } );
+  pending_callbacks_.erase( it );
+}
+
+int ServiceClient::generateInternalCallbackId()
+{
+  // Create a unique incrementing internal goal ID
+  static std::atomic<int> current_id = 0;
+  return current_id.fetch_add( 1 );
 }
 } // namespace qml_ros2_plugin
